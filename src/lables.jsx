039806eb16beee468765cd5499a3d13bd4874fe8
Diego Antonio <suporte.diegociara@gmail.com>
2023-08-22 12:54:05 -0300

    update overview

--- src/lables.jsx
+++ src/lables.jsx
@@ -63,6 +63,63 @@ export const HomePage = [
   },
 ];
 
+export const Overview = [
+  {
+    head:"OverView",
+    title:"Coloque seu título aqui",
+    divisor: false,
+    subtitle:"Veja como fica o paragrafo abaixo",
+    paragraph:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium deserunt, dolorem quia sequi fuga modi libero voluptatem eaque dolore aperiam, nemo ab architecto labore ut non autem facilis! Voluptatibus, rem.",
+    phrase:"Funil de vendas simples",
+    code: null,
+  },
+  {
+    head:"Esse é um container só com o head ",
+    title: null,
+    subtitle: null,
+    divisor: false,
+    paragraph:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium deserunt, dolorem quia sequi fuga modi libero voluptatem eaque dolore aperiam, nemo ab architecto labore ut non autem facilis! Voluptatibus, rem.",
+    phrase:"Funil de vendas simples",
+    code: null,
+  },
+  {
+    head:"Bem vindo ao",
+    title:"Faça a gestão de suas vendas:",
+    subtitle:"Usuário único",
+    divisor: false,
+    paragraph:"Funil de vendas simples",
+    phrase:"Funil de vendas simples",
+    code:"docker-compose up-d",
+  },
+  {
+    head:"Bem vindo ao Projeto",
+    title:"Coloque seu título aqui",
+    divisor: false,
+    subtitle:"Veja como fica o paragrafo abaixo",
+    paragraph:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium deserunt, dolorem quia sequi fuga modi libero voluptatem eaque dolore aperiam, nemo ab architecto labore ut non autem facilis! Voluptatibus, rem.",
+    phrase:"Funil de vendas simples",
+    code: null,
+  },
+  {
+    head:"Esse é um container só com o head ",
+    title: null,
+    subtitle: null,
+    divisor: false,
+    paragraph:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium deserunt, dolorem quia sequi fuga modi libero voluptatem eaque dolore aperiam, nemo ab architecto labore ut non autem facilis! Voluptatibus, rem.",
+    phrase:"Funil de vendas simples",
+    code: null,
+  },
+  {
+    head:"Bem vindo ao",
+    title:"Faça a gestão de suas vendas:",
+    subtitle:"Usuário único",
+    divisor: false,
+    paragraph:"Funil de vendas simples",
+    phrase:"Funil de vendas simples",
+    code:"docker-compose up-d",
+  },
+];
+
 export const ContentCards = [
   {
     title: 'Primeiro Card',
--- src/pages/overview.jsx
+++ src/pages/overview.jsx
@@ -1,8 +1,23 @@
-import { ContentPage } from "../ui/styles/Content";
+import { Overview } from "../lables";
+import { Contents } from "../ui/components/Contents";
+import { Divisor, Page, PageContainer } from "../ui/styles/Container";
 
 export function OverView() {
   return(
-    <ContentPage>
-    </ContentPage>
+    <Page>
+      <PageContainer>
+      {Overview.map((card)=>(
+        <Contents
+          h1={card.head}
+          h2={card.title}
+          h3={card.subtitle}
+          div={card.divisor}
+          p={card.paragraph}
+          span={card.phrase}
+          code={card.code}
+        />
+        ))}
+      </PageContainer>
+    </Page>
   )
 }
