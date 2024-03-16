756552d5a7c42c33900ac1fefd82c717413adb43
Diego Antonio <suporte.diegociara@gmail.com>
2024-03-15 21:28:17 -0300

    update

--- src/services/ServiceApi.jsx
+++ src/services/ServiceApi.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 //DECLARA O AXIOS PARA USAR O BACK END NA PORTA 3333 OU EM UMA PASSA POR PROD
 
 export const serviceApi = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://asaas.saasforce.com.br',
   // baseURL: 'https://uniao-filial.figio.com.br',
 });
 
